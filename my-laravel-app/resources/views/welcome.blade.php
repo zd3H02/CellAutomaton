89f539cd747be9554948a4d50bd272b4ae68fc3f
zd3H02 <zd3h02@gmail.com>
2020-03-25 21:22:02 +0900

    寝る前のコミット

--- my-laravel-app/resources/views/welcome.blade.php
+++ my-laravel-app/resources/views/welcome.blade.php
@@ -14,6 +14,17 @@
 
         <!-- Styles -->
         <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+        <!-- Favicon Generator-->
+        <link rel="apple-touch-icon" sizes="180x180" href="/image/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/image/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/image/favicon-16x16.png">
+        <link rel="manifest" href="/image/site.webmanifest">
+        <link rel="mask-icon" href="/image/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
+
+
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
